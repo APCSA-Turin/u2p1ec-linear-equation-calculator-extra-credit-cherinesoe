d53b81308fc34ff8760e8af2bf60536a377c7f3d
cherinesoe <148895697+cherinesoe@users.noreply.github.com>
2024-10-17 13:16:47 +0000

    Ec

---
+++ src/main/java/com/example/project/Main.java
@@ -0,0 +1,7 @@
+package com.example.project;
+public class Main {
+    public static void main(String[] args) {
+        LinearCalculator c = new LinearCalculator("(3,4)","(3,-4)");
+        System.out.println(c.printInfo());
+    }
+}
